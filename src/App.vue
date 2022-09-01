df1143d4688942404dfe6db9740c0d003eb4ae94
eugenie desaunai <eugeniedesaunai@gmail.com>
2022-09-01 14:18:42 +0200

    app.vue modif

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
@@ -11,12 +7,11 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #131E28;
 }
 
-nav {
-  padding: 30px;
+.texteColor {
+  color: #131E28;
 }
 
 nav a {
