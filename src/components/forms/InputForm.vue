f82097e0e60e789cffdb08d03ec456463105a203
Jonathan Bourree <jonathan.bourree@aden.com>
2022-09-01 16:18:16 +0200

    Formulaire d'ajout de recette

---
+++ src/components/forms/InputForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <label :for=fname>{{ content }}</label>
+        <input v-model="value" @change="SelectInput" type="text" :id=fname :name=fname>
+    </div>
+
+</template>
+<script>
+export default {
+    props: {
+        fname: String,
+        content: String
+    },
+    methods: {
+        SelectInput() {
+            this.$emit('selectInput', this.value);
+        }
+    },
+
+}
+</script>
+<style>
+</style>
---
+++ src/components/forms/SelectForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <label :for=fname>{{ content }}</label>
+        <select v-model="selected" @change="SelectOption" :name=fname :id=fname>
+            <option v-for="(option, index) in options" :value="option" :key="index">{{ option }}</option>
+        </select>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        fname: String,
+        content: String,
+        options: Array
+    },
+    methods: {
+        SelectOption() {
+            this.$emit('selectOption', this.selected)
+        }
+    }
+}
+</script>
+<style>
+</style>
