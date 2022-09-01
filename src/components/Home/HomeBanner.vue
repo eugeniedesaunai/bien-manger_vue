f07e5ff2f2895b9fe814cf89db928294f63988da
eugenie desaunai <eugeniedesaunai@gmail.com>
2022-09-01 20:53:00 +0200

    nav+banner tablette/ordi ok

--- src/components/Home/HomeBanner.vue
+++ src/components/Home/HomeBanner.vue
@@ -21,29 +21,55 @@ export default {
 }
 
 .textBanner {
-    background-color: rgba(29, 40, 75, 0.5);
-    color: #E0F2C4;
     position: absolute;
     text-align: center;
     padding: 2rem;
     top: 25%;
     margin: 0;
     border-radius: 1rem;
-    margin-left: 2rem;
-    margin-right: 2rem;
+    width: 80vw;
+    left: 10%;
+    background-color: rgba(224, 242, 196, 0.7);
+    color: #2c3e50;
 }
 
 .buttonSaison {
     position: absolute;
-    top: 65%;
+    top: 68%;
     left: 20%;
     margin: 0;
-    background-color: rgba(29, 40, 75, 0.5);
-    color: #E0F2C4;
     border-radius: 1rem;
     width: 60vw;
     height: 8vh;
     text-transform: uppercase;
     white-space: normal;
+    background-color: rgba(29, 40, 75, 0.7);
+    color: #E0F2C4;
+}
+
+@media(min-width: 768px) {
+    .textBanner {
+        width: 50%;
+        top: 30%;
+        left: 40%;
+    }
+
+    .buttonSaison {
+        top: 70%;
+    }
+}
+
+@media(min-width: 768px) {
+    .textBanner {
+        width: 30%;
+        top: 25%;
+        left: 65%;
+    }
+
+    .buttonSaison {
+        width: 25vw;
+        top: 58%;
+        left: 68%;
+    }
 }
 </style>
