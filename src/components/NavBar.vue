25af466fa9662036e058eb0e58dcd5e0dd159c0d
eugenie desaunai <eugeniedesaunai@gmail.com>
2022-09-01 14:15:07 +0200

    Navbar OK, Nav Accueil ok, ajout fichier js

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,31 +1,52 @@
 <template lang="">
-<nav class="colorNavbar">
+<nav :class="className">
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo center texteColor">CuisiSaison</a>
-      <ul id="nav-mobile" class="left hide-on-med-and-down flexArround">
+      <a href="#!" class="brand-logo center texteColor">CuisiSaison</a>
+     <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="left  hide-on-med-and-down flexArround">
         <li ><a class="texteColor" href="">Saison</a></li>
         <li><a class="texteColor" href="">Formulaire</a></li>
       </ul>
     </div>
   </nav>
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="">Saison</a></li>
+    <li><a href="">Formulaire</a></li>
+  </ul>
 </template>
 <script>
 export default {
-    name: "NavBar"
+  name: "NavBar",
+  props: {
+    isHomePage: { type: Boolean, default: false }
+  },
+  computed: {
+    className() {
+      return this.isHomePage ? "navAccueil" : "colorNavbar";
+    }
+  },
+  methods: {
+  }
 }
+
 </script>
 <style scoped>
 .colorNavbar {
-    background-color: #E0F2C4;
+  background-color: #E0F2C4;
+}
+
+.navAccueil {
+  position: absolute;
+  background-color: rgba(224, 242, 196, 0.2);
 }
 
 .brand-logo {
-    color: #131E28;
+  color: #131E28;
 }
 
 .left {
-    display: flex;
-    justify-content: space-around;
-    float: none !important;
+  display: flex;
+  justify-content: space-around;
+  float: none !important;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <NavBar :isHomePage="true"></NavBar>
+  <HomeBanner></HomeBanner>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue';
+import HomeBanner from '@/components/Home/HomeBanner.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    NavBar,
+    HomeBanner
   }
 }
 </script>
+
+<style>
+</style>
