42fd48b3b7ef7c66fb0ae92012bd44771f74510d
Brandon <brandon.messager@gmail.com>
2022-09-01 14:15:47 +0200

    ajout carousel et bouton saison

---
+++ src/components/SearchBySeason/IngredientCarousel.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="carousel">
+    <a class="carousel-item" href="#one!">patate</a>
+    <a class="carousel-item" href="#two!">tomate</a>
+    <a class="carousel-item" href="#three!">beurre</a>
+    <a class="carousel-item" href="#four!">chocolat</a>
+  </div>
+</template>
+
+    <script>
+        export default {
+            name: 'IngredientCarousel',
+            data() {
+                return {
+                }
+            },
+            props: {
+            }
+        }
+    </script>
+ <style scoped>
+ .carousel{
+     margin:auto;
+     width:100%;
+ }
+ .carousel-item{
+     left:auto;
+ }
+ </style>
--- src/views/SearchBySeason.vue
+++ src/views/SearchBySeason.vue
@@ -1,17 +1,22 @@
 <template>
-  <div >
-      yo
+  <div>
+        <SeasonSelection>
+        </SeasonSelection>
+        <IngredientCarousel>
+        </IngredientCarousel>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import SeasonSelection from '@/components/SearchBySeason/SeasonSelection.vue'
+import IngredientCarousel from '@/components/SearchBySeason/IngredientCarousel.vue'
 
 export default {
   name: 'SearchBySeason',
   components: {
-    SeasonSelection
+    SeasonSelection,
+    IngredientCarousel
   }
 }
 </script>
