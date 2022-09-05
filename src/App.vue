128779f1a6e849f3f45c0972afc5c6ea7d0933f6
eugenie desaunai <eugeniedesaunai@gmail.com>
2022-09-05 15:01:59 +0200

    modification SearchAndMounthRecipe

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-        <div class='backgroundStatics'></div>
-        <router-view/>
+  <div class='backgroundStatics'></div>
+  <router-view />
 </template>
 
 <style>
@@ -60,71 +60,71 @@
 }
 
 body {
-        background-color: D9D3CC;
-        margin: 0;
-        overflow-x: hidden;
-        overflow-y: hidden;
-    }
+  background-color: D9D3CC;
+  margin: 0;
+  overflow-x: hidden;
 
-    .backgroundStatics {
-        position: fixed;
-        z-index:-1;
-        top: -50%;
-        left: -50%;
-        right: -50%;
-        bottom: -50%;
-        width: 200%;
-        height: 200vh;
-        background: transparent url('http://assets.iceable.com/img/noise-transparent.png') repeat 0 0;
-        background-repeat: repeat;
-        animation: bg-animation .2s infinite;
-        opacity: .9;
-        visibility: visible;
-    }
+}
 
-    @keyframes bg-animation {
-        0% {
-            transform: translate(0, 0)
-        }
+.backgroundStatics {
+  position: fixed;
+  z-index: -1;
+  top: -50%;
+  left: -50%;
+  right: -50%;
+  bottom: -50%;
+  width: 200%;
+  height: 200vh;
+  background: transparent url('http://assets.iceable.com/img/noise-transparent.png') repeat 0 0;
+  background-repeat: repeat;
+  animation: bg-animation .2s infinite;
+  opacity: .9;
+  visibility: visible;
+}
 
-        10% {
-            transform: translate(-5%, -5%)
-        }
+@keyframes bg-animation {
+  0% {
+    transform: translate(0, 0)
+  }
 
-        20% {
-            transform: translate(-10%, 5%)
-        }
+  10% {
+    transform: translate(-5%, -5%)
+  }
 
-        30% {
-            transform: translate(5%, -10%)
-        }
+  20% {
+    transform: translate(-10%, 5%)
+  }
 
-        40% {
-            transform: translate(-5%, 15%)
-        }
+  30% {
+    transform: translate(5%, -10%)
+  }
 
-        50% {
-            transform: translate(-10%, 5%)
-        }
+  40% {
+    transform: translate(-5%, 15%)
+  }
 
-        60% {
-            transform: translate(15%, 0)
-        }
+  50% {
+    transform: translate(-10%, 5%)
+  }
 
-        70% {
-            transform: translate(0, 10%)
-        }
+  60% {
+    transform: translate(15%, 0)
+  }
 
-        80% {
-            transform: translate(-15%, 0)
-        }
+  70% {
+    transform: translate(0, 10%)
+  }
 
-        90% {
-            transform: translate(10%, 5%)
-        }
+  80% {
+    transform: translate(-15%, 0)
+  }
 
-        100% {
-            transform: translate(5%, 0)
-        }
-    }
+  90% {
+    transform: translate(10%, 5%)
+  }
+
+  100% {
+    transform: translate(5%, 0)
+  }
+}
 </style>
