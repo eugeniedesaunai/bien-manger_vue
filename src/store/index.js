356ee7c3001fb20253640c56c078f544245887a8
Jonathan Bourree <jonathan.bourree@aden.com>
2022-09-06 16:08:49 +0200

    Optimization n°2 du store

--- src/store/index.js
+++ src/store/index.js
@@ -11,53 +11,52 @@ export default createStore({
     recipes_ingredients: {}
   },
   getters: {
-
   },
   mutations: {
     init() {
       this.state.myheader.append("Authorization", "Bearer " + process.env.VUE_APP_AIRTABLE_API_KEY);
       this.state.options = { headers: this.state.myheader };
     },
-    storeRecipes(context, recipes) {
-      context.recipes = Object.assign({}, recipes);
-      console.log(context.recipes);
-    },
-    storeSeasons(context, seasons) {
-      context.seasons = Object.assign({}, seasons);
-      console.log(context.seasons);
-    },
-    storeIngredients(context, ingredients) {
-      context.ingredients = Object.assign({}, ingredients);
-      console.log(context.ingredients);
-    },
-    storeSteps(context, steps) {
-      context.steps = Object.assign({}, steps);
-      console.log(context.steps);
-    },
-    storeRecipesIngredients(context, recipes_ingredients) {
-      context.recipes_ingredients = Object.assign({}, recipes_ingredients);
-      console.log(context.recipes_ingredients);
+    /**
+     * @param itemList : resultat de la récupération des données 
+     * @param statename : nom du state correspondant à la table
+     * [recipes, seasons, steps, ingredients, recipes_ingredients]
+     */
+    storeItems(context, elements) {
+      let itemList = '';
+      let statename = '';
+      for (let index = 0; index < elements.length; index++) {
+        itemList = elements[0];
+        statename = elements[1];
+      }
+      statename;
+      context.statename = Object.assign({}, itemList);
+      console.log(context.statename);
     }
   },
   actions: {
     /**
-     * url: [Recette, Saison, Etape, Ingredient, Recette_has_Ingredients]
+     * @param tablename : nom de la table
+     * [Recette, Saison, Etape, Ingredient, Recette_has_Ingredients]
+     * @param maxrecords: nombre de données dans la table
+     * @param statename : nom du state correspondant à la table
+     * appel de la fonction: this.$store.dispatch('getItems', [tablename, maxrecords, statename])
      */
     getItems(context, elements) {
-      let url = '';
+      let tablename = '';
       let maxrecords = 0;
-      let name = '';
+      let statename = '';
       for (let index = 0; index < elements.length; index++) {
         console.log(elements[index]);
-        url = elements[0];
+        tablename = elements[0];
         maxrecords = elements[1];
-        name = elements[2];
+        statename = elements[2];
       }
       context.commit('init');
-      fetch("https://api.airtable.com/v0/appcCZi8CySwsHTVJ/" + url + "?maxRecords=" + maxrecords + "&view=Grid%20view", this.state.options)
+      fetch("https://api.airtable.com/v0/appcCZi8CySwsHTVJ/" + tablename + "?maxRecords=" + maxrecords + "&view=Grid%20view", this.state.options)
         .then((data) => data.json())
         .then((result) => {
-          this.commit('store' + name, result);
+          this.commit('storeItems', [result, statename]);
         });
     }
   },
--- src/views/RecipeShow.vue
+++ src/views/RecipeShow.vue
@@ -14,11 +14,6 @@ export default {
         NavBar
     },
     created() {
-        //this.$store.dispatch('getRecipes', ['Recette', 3, 'Recipes']);
-        this.$store.dispatch('getItems', ['Recette_has_Ingredient', 10, 'RecipesIngredients']);
-        //this.$store.dispatch('getIngredients');
-        //this.$store.dispatch('getSteps');
-        //this.$store.dispatch('getRecipesIngredients');
     },
 }
 </script>
