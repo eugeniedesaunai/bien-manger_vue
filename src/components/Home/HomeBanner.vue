fae938556d0a0a396157528f1d68885bc5f75740
eugenie desaunai <eugeniedesaunai@gmail.com>
2022-09-06 14:36:57 +0200

    mere

--- src/components/Home/HomeBanner.vue
+++ src/components/Home/HomeBanner.vue
@@ -11,7 +11,6 @@
 
 export default {
     name: 'HomeBanner',
-
 }
 </script>
 
--- src/views/RecipeShow.vue
+++ src/views/RecipeShow.vue
@@ -1,5 +1,10 @@
 <template lang="">
     <NavBar></NavBar>
+    <article>
+        <div class="imgRecipe"></div>
+        <p></p>
+    </article>
+    
 </template>
 <script>
 import NavBar from '@/components/NavBar.vue';
@@ -8,19 +13,30 @@ export default {
     components: {
         NavBar
     },
-    created() {
-        var myHeaders = new Headers();
-        //myHeaders.append("Authorization", "Bearer " + process.env.VUE_APP_AIRTABLE_API_KEY);
-        let options = {
-            headers: myHeaders
-        }
-        fetch("https://api.airtable.com/v0/appcCZi8CySwsHTVJ/Recette?maxRecords=3&view=Grid%20view", options)
-            .then((data) => data.json())
-            .then((recipes) => {
-                console.log(recipes);
-            });
-    },
 }
 </script>
+
 <style scoped>
+article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+div {
+    width: 50vw;
+    height: 50vh;
+    background-size: cover;
+    background-position: center;
+}
+
+.imgRecipe {
+    background-image: url(../assets/Home/farine.jpg);
+}
+
+p {
+    width: 60vw;
+    height: 20vh;
+    background-color: #E0F2C4;
+}
 </style>
