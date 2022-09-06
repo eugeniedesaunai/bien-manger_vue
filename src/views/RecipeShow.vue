348543b97dcaaf4b00cf83782ca871a0d2db2aa8
Jonathan Bourree <jonathan.bourree@aden.com>
2022-09-06 14:26:26 +0200

    mise en place du store

--- src/views/RecipeShow.vue
+++ src/views/RecipeShow.vue
@@ -9,16 +9,19 @@ export default {
         NavBar
     },
     created() {
-        var myHeaders = new Headers();
-        //myHeaders.append("Authorization", "Bearer " + process.env.VUE_APP_AIRTABLE_API_KEY);
-        let options = {
-            headers: myHeaders
-        }
-        fetch("https://api.airtable.com/v0/appcCZi8CySwsHTVJ/Recette?maxRecords=3&view=Grid%20view", options)
-            .then((data) => data.json())
-            .then((recipes) => {
-                console.log(recipes);
-            });
+        //this.$store.dispatch('getRecipes', ['DWWM', 'toto']);
+        this.$store.dispatch('getSeasons', 'DWWM');
+        //this.$store.commit('test', 'DWWM');
+        // var myHeaders = new Headers();
+        // //myHeaders.append("Authorization", "Bearer " + process.env.VUE_APP_AIRTABLE_API_KEY);
+        // let options = {
+        //     headers: myHeaders
+        // }
+        // fetch("https://api.airtable.com/v0/appcCZi8CySwsHTVJ/Recette?maxRecords=3&view=Grid%20view", options)
+        //     .then((data) => data.json())
+        //     .then((recipes) => {
+        //         console.log(recipes);
+        //     });
     },
 }
 </script>
