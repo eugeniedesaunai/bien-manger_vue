e7ad725b90f250ede85d72016d5cf91f1c988164
eugenie desaunai <eugeniedesaunai@gmail.com>
2022-09-13 11:20:31 +0200

    component IngredientSelect

---
+++ src/components/forms/IngredientSelect.vue
@@ -0,0 +1,25 @@
+<template lang="">
+    <div>
+        <select  name="" id="" >
+            <option v-for="(item, index) in name" :value= "index" :key="item.id">{{item.name}}</option>
+        </select> 
+
+    </div>
+</template>
+<script>
+export default {
+    name: "IngredientSelect",
+    data() {
+        return {
+            name: []
+        }
+    },
+    created() {
+        this.$store.dispatch('ingredient/checkIngredient');
+        this.name = this.$store.getters['ingredient/listIngredient'];
+    },
+}
+</script>
+<style lang="">
+    
+</style>
--- src/views/RecipeShow.vue
+++ src/views/RecipeShow.vue
@@ -2,20 +2,22 @@
     <NavBar></NavBar>
     <article>
         <div class="imgRecipe"></div>
-        <p>{{this.$store.getters['season/getSeasons']('Eté')}}</p>
-        <select>
+       <p>{{this.$store.getters['season/getSeasons']('Eté')}}</p> 
+       <IngredientSelect></IngredientSelect>
+      <!--   <select>
             <option v-for="(item, index) in name" :value="index" :key="item.id">{{item.name}}</option>
         </select>
         {{this.$store.getters['season/getName']}}
        <select  name="" id="" >
             <option v-for="(item, index) in name" :value= "index" :key="item.id">{{item.name}}</option>
         </select> 
-        <p> {{this.$store.getters['ingredient/listIngredient']}}</p>
+        <p> {{this.$store.getters['ingredient/listIngredient']}}</p> -->
     </article>
     
 </template>
 <script>
 import NavBar from '@/components/NavBar.vue';
+import IngredientSelect from "@/components/forms/IngredientSelect.vue"
 
 export default {
     name: "RecipeShow",
@@ -26,7 +28,8 @@ export default {
         }
     },
     components: {
-        NavBar
+        NavBar,
+        IngredientSelect
     },
     methods: {
 
@@ -38,11 +41,10 @@ export default {
     created() {
         this.$store.dispatch('season/checkSeason');
         this.$store.dispatch('meal/checkMeal');
-        this.name = this.$store.getters['ingredient/listIngredient']
+        this.name = this.$store.getters['ingredient/listIngredient'],
+            this.$store.dispatch('ingredient/checkIngredient');
     },
-    created() {
-        this.$store.dispatch('ingredient/checkIngredient');
-    },
+
 }
 </script>
 
