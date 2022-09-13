95cc9084cd64191e8d9a534591b3da106f95a2bd
Brandon <brandon.messager@gmail.com>
2022-09-13 11:07:34 +0200

    tableau fetch recette

--- src/store/recipe.js
+++ src/store/recipe.js
@@ -3,6 +3,7 @@ export default {
     namespaced: true,
     state: {
         recipe: {},
+        recipeIngredient: {},
     },
     getters: {
         getSeason(state, name) {
@@ -11,27 +12,47 @@ export default {
     },
     mutations: {
         fillRecipe() {
-            let tempRecipe = [];
-            let tempStep = [];
-            let tempIngredient = [];
-            let tempSeason = [];
+            let temp = [];
+            let tempIngrdients = [];
+            let ingredients = this.state.recipeIngredient;
             let item = this.state.recipe;
-            console.log(item);
-            Object.keys(item).forEach(key => {
-                Object.keys(item[key]).forEach(child => {
-                    tempStep.push(item[key][child].fields.Etape);
-                    tempSeason.push(item[key][child].fields.Saison);
-                    tempIngredient.push(item[key][child].fields.Recette_has_Ingredient);
-                    tempRecipe.push({ id: item[key][child].id, name: item[key][child].fields.Name, description: item[key][child].fields.Description });
+            Object.keys(item).forEach(keyRecipe => {
+                Object.keys(item[keyRecipe]).forEach(childRecipe => {
+
+                    Object.keys(ingredients).forEach(keyIngredient => {
+                        Object.keys(ingredients[keyIngredient]).forEach(childIngredient => {
+                            Object.keys(ingredients[keyIngredient][childIngredient].fields.Recette).forEach(recipeId => {
+                                if (ingredients[keyIngredient][childIngredient].fields.Recette[recipeId] == item[keyRecipe][childRecipe].id) {
+                                    tempIngrdients.push({
+                                        ingredientIds: ingredients[keyIngredient][childIngredient].fields.Ingredient,
+                                        quantity: ingredients[keyIngredient][childIngredient].fields.Quantity,
+                                        unit: ingredients[keyIngredient][childIngredient].fields.Unit
+                                    })
+                                }
+                            });
+                        })
+                    })
+                    temp.push({
+                        Recipe: {
+                            id: item[keyRecipe][childRecipe].id,
+                            name: item[keyRecipe][childRecipe].fields.Name,
+                            description: item[keyRecipe][childRecipe].fields.Description,
+                            ingredient: tempIngrdients,
+                            step: item[keyRecipe][childRecipe].fields.Etape,
+                            season: item[keyRecipe][childRecipe].fields.Saison
+                        }
+                    });
                 })
             })
-            console.log(tempRecipe);
+            this.state.recipe = temp;
         }
     },
     actions: {
         async checkRecipe(context) {
-            let result = await api.find({ resource: 'Recette', query: 'maxRecords=10' })
+            let result = await api.find({ resource: 'Recette', query: 'maxRecords=10' });
             this.state.recipe = result;
+            result = await api.find({ resource: 'Recette_has_Ingredient', query: 'maxRecords=30' });
+            this.state.recipeIngredient = result;
             context.commit('fillRecipe');
         }
     }
