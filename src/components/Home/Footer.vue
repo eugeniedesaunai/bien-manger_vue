2ec8da8ae2e9a221108aed3f1d34409c64901abd
eugenie desaunai <eugeniedesaunai@gmail.com>
2022-09-15 22:54:50 +0200

    footer ok

--- src/components/Home/Footer.vue
+++ src/components/Home/Footer.vue
@@ -1,6 +1,9 @@
 <template lang="">
-    <div class="bgGreyColor">
-        
+    <div class="bgGreyColor taille textCenter">
+        <p>Concocté avec ♥ par Cuisisaison</p>
+        <p>Tous droits réservés Marmiton.org - 1872-2022</p>
+        <p>Recrutement ● Mentions légales ● Conditions Générales d'Utilisation ● Contact ● FAQ ● Politique de protection des données personnelles ● Paramétrer les cookies ● Foire aux questions - Vos choix concernant l'utilisation de cookies</p>
+        <p>Pour des milliers de bêtises: lesjoiesducode.fr</p>
     </div>
 </template>
 <script>
@@ -9,8 +12,12 @@ export default {
 }
 </script>
 <style scoped>
+.taille {
+    font-size: 0.6rem;
+}
+
 div {
     width: 100vw;
-    height: 10vh;
+    height: 12vh;
 }
 </style>
